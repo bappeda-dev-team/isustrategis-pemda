8f58bd9b519da1e087b1bad5adef4e15d2a1355d
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2025-07-26 02:58:54 +0700

    hapus cors

--- app/router.go
+++ app/router.go
@@ -2,7 +2,6 @@ package app
 
 import (
 	"isustrategisService/controller"
-	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -14,16 +13,6 @@ func NewRouter(csfController controller.CsfController, outcomeController control
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
-	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"*"}, // Atau spesifik: []string{"http://localhost:3000"}
-		AllowMethods: []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete, http.MethodOptions},
-		AllowHeaders: []string{
-			echo.HeaderOrigin,
-			echo.HeaderContentType,
-			echo.HeaderAccept,
-			echo.HeaderAuthorization,
-		},
-	}))
 
 	e.GET("/swagger/*", echoSwagger.WrapHandler)
 
